5d47da4fb65354bcff2398fabeb8e8b82e119e6e
Sugumaran <113161153+Sugumaran-RnD@users.noreply.github.com>
2025-09-12 12:35:57 +0530

    Update test_led.c

--- tests/test_led.c
+++ tests/test_led.c
@@ -1,1 +1,13 @@
 // test led
+#include <assert.h>
+#include "led.h"
+
+int main(void) {
+    led_on();
+    assert(led_status() == 1);
+
+    led_off();
+    assert(led_status() == 0);
+
+    return 0;
+}
